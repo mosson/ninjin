4a23e58f2c8b5e212bd5511b195a88ce0bf89275
a-munakata <a.munakata@4digit.jp>
2013-06-28 20:47:20 +0900

    PUSH

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,13 +1,12 @@
 class Log < ActiveRecord::Base
-
 	paginates_per 10
 
 	attr_accessible :entry, :occurred_date, :environment, :error_status, :is_issued, :is_closed, :is_updated, :ip_address
 	default_scope { order("occurred_date DESC") }
-	scope :is_issued, where(:is_issued => true)
-	scope :is_closed, where(:is_closed => true)
+	scope :is_issued, 			where(:is_issued => true)
+	scope :is_closed, 			where(:is_closed => true)
 	scope :envs_production, where(:environment => "production")
-	scope :envs_staging, where(:environment => "staging")
-	scope :envs, 			where(:environment => "staging")
+	scope :envs_staging,		where(:environment => "staging")
+	scope :envs, 						where(:environment => @environment)
   
 end
