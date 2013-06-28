5bf6c745f090f9c4314b7171f503697fc45115fa
a-munakata <a.munakata@4digit.jp>
2013-06-28 20:46:42 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 LogViewerApp::Application.routes.draw do
-  
-  match ':controller(/:action(/:id))(.:format)'
 
-  resources :environment
 
-  # match '/login' => 'sessions#new', via: :get
-  # match '/login' => 'sessions#create', via: :post
-  # match '/logout' => 'sessions#destroy', via: :delete
+
+	post :check,										:to => 'log#check', 				  as: 'check_close'
+	get :check, 										:to => 'log#check', 					as: 'check_close'
+
+
+	match '/:environment',   					:to => 'log#index', 					as: 'environment' do
+		get '/:environment',   					:to => 'log#index', 					as: 'environment#index'
+	end
+	
+	get '/:environment/closed', 			:to => 'log#index',        		as: 'close'
+	post '/:environment/closed', 			:to => 'log#open_or_closed', 	as: 'close'
+
+	get '/:environment/open', 	  		:to => 'log#index', 					as: 'open'
+	post '/:environment/open', 	  		:to => 'log#open_or_closed', 	as: 'open'
+
+	match '/:environment/:invalid', 	:to => 'log#invalid',					as: 'redirect'
+
+	
+
 end
