05ee3de70cf6d982b079549fe528b85e706d229c
a-munakata <a.munakata@4digit.jp>
2013-06-28 14:49:59 +0900

    environmentコントローラーの機能をlogコントローラーに移動

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -2,6 +2,25 @@
 
 class LogController < ApplicationController
 	def index
-		render :text => "hello"
+		@backtrace   = params[:backtrace]
+		@environment = params[:env]
+		@status_code = params[:status_code]	
+		@date_from   = params[:date_from]
+		@date_to     = params[:date_to]
+		@closed      = params[:closed]
+		@open      	 = params[:open]
+		@ip_address	 = params[:ip_address]
+		
+		@logs = Log.all
+
+		respond_to do |format|
+			format.html
+		end
+	end
+
+	def view
+		@logs = Log.all
+		render :action => "index", :layout => "environment"				
 	end
 end
+
