35ed927e1862f2719b451b6ee4c1eb34820b8554
a-munakata <a.munakata@4digit.jp>
2013-07-02 14:54:08 +0900

    resourcesでroutingを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 LogViewerApp::Application.routes.draw do
 
-	match '/:environment',   					:to => 'log#index', 					as: 'environment' do
-		get '/:environment',   					:to => 'log#index', 					as: 'environment#index'
+	resources :logs , :to => 'logs#index', :only => %w(index) do
+		collection do
+			get '/:environment', as: 'environment'
+
+			post :check,	  as: 'check_close'
+			get :check, 		as: 'check_close'			
+		end
 	end
-
-
-	post :check,										:to => 'log#check', 				  as: 'check_close'
-	get :check, 										:to => 'log#check', 					as: 'check_close'
 	
-	get '/:environment/closed', 			:to => 'log#index',        		as: 'close'
-	post '/:environment/closed', 			:to => 'log#index', 					as: 'close'
-
-	get '/:environment/open', 	  		:to => 'log#index', 					as: 'open'
-	post '/:environment/open', 	  		:to => 'log#index', 					as: 'open'
-
-	match '/:environment/:invalid', 	:to => 'log#invalid',					as: 'redirect'
+	# match '/:environment/:invalid', 	:to => 'logs#invalid',					as: 'redirect'
 
 	
 
