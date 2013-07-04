6c53f00a0b14f9956b3b116fd4ff01592025f317
a-munakata <a.munakata@4digit.jp>
2013-07-04 15:03:27 +0900

    リファクタリング：controller整理

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,25 +3,21 @@
 class LogsController < ApplicationController	
 
 	def index		
-		@environment = params[:environment]		
 		@logs = Log.scoped
 
 		@logs = @logs.envs(@environment).page(params[:page]).per(10) unless @environment.nil?
 
-		@logs = @logs.where(:is_closed => !params[:log][:is_closed].nil?) unless params[:log].nil?
+		@logs = @logs.where(:is_closed => params[:log][:is_closed].nil?) unless params[:log].present?
 
 		respond_to do |format|
 			format.html
 		end		
 	end
 
-	def invalid		
-		# redirect_to "/staging" if params[:invalid] == "staging"
-		# redirect_to "/production" if params[:invalid] == "production"
+	def invalid
 	end
 
-	def check
-		
+	def check		
 	end	
 
 end
