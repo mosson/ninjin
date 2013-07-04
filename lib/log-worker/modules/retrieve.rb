d4237238a1bd3ce8854b295b2b2e40d29b93ec92
a-munakata <a.munakata@4digit.jp>
2013-07-04 15:11:36 +0900

    rakeタスク:ロジック変更
    unicornログはunicornディレクトリに移動

--- lib/log-worker/modules/retrieve.rb
+++ lib/log-worker/modules/retrieve.rb
@@ -7,7 +7,7 @@ module Retrieve
 		conf = PathFactory.new.conf
 		current_dir = Dir.getwd
 		usages			= ["backup"]
-		
+
 
 		Dir.mkdir("#{current_dir}/backup")
 		conf.keys.each do |env|
@@ -23,6 +23,7 @@ module Retrieve
 
 		conf.each do |key, val|
 			Net::SSH.start(val["path"], val["user"], {:password => val["password"]}) do |ssh|
+				puts "ssh successfully connected to #{key}"
 				target_files["#{key}"] = ssh.exec!("ls #{ENV["PATH_TO_LOGS"]}").split("\n")
 			end
 		end
@@ -36,9 +37,9 @@ module Retrieve
 							FileUtils.mkdir_p("#{dir_path}/#{target_env}")
 							scp.download! file, "#{dir_path}/#{target_env}"
 							Dir::entries("#{dir_path}/#{target_env}").each do |file|
-								
+
 								system("gunzip #{dir_path}/#{target_env}/#{file}") if File::ftype("#{dir_path}/#{target_env}/#{file}") == "file" || file.match(/.gz/)
-								
+
 							end
 						}
 					end
@@ -52,15 +53,30 @@ module Retrieve
 		puts "started organizing files..."
 		conf = PathFactory.new.conf
 		pattern = [/[0-9]{6}/, /unicorn/, /nginx/]
+		
 		conf.keys.each do |env|
 			Dir::entries("#{dir_path}/#{env}").each do |file|
+								
+				FileUtils.mkdir_p("#{dir_path}/#{env}/#{file.match(pattern[0])}") unless Dir.exist?("#{dir_path}/#{env}/#{file.match(pattern[0])}")
+				
 				if file.match(pattern[0])
-					FileUtils.mkdir_p("#{dir_path}/#{env}/#{file.match(pattern[0])}") unless Dir.exist?("#{dir_path}/#{env}/#{file.match(pattern[0])}")
-					system("mv #{dir_path}/#{env}/#{file} #{dir_path}/#{env}/#{file.match(pattern[0])}/#{file}") unless file.match(/.gz/)
+					if File.exist?("#{dir_path}/#{env}/#{file}") && !file.match(/gz/)
+						system("mv #{dir_path}/#{env}/#{file} #{dir_path}/#{env}/#{file.match(pattern[0])}/#{file}") if file.match(/log/)						
+					end
 				end
-
-				if File::ftype("#{dir_path}/#{env}/#{file}")
+			end
+			Dir::entries("#{dir_path}/#{env}").each do |dir|
+				Dir::entries("#{dir_path}/#{env}/#{dir}").each do |file|
+					FileUtils.mkdir_p("#{dir_path}/#{env}/#{dir}/#{file.match(pattern[1])}") unless Dir.exist?("#{dir_path}/#{env}/#{dir}/#{file.match(pattern[1])}")
+				
+					if file.match(pattern[1])
+						if File.exist?("#{dir_path}/#{env}/#{dir}/#{file}") && !file.match(/gz/)
+							system("mv #{dir_path}/#{env}/#{dir}/#{file} #{dir_path}/#{env}/#{dir}/#{file.match(pattern[1])}/#{file}") if file.match(/log/)
+						end
+					end	
 				end
 			end
 		end
+
 	end
+end
