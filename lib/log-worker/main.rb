9ce69be5bebbd15a9d37461da9aec3e5d2667423
a-munakata <a.munakata@4digit.jp>
2013-07-04 23:45:44 +0900

    reportタスク追加

--- lib/log-worker/main.rb
+++ lib/log-worker/main.rb
@@ -1,6 +1,8 @@
 require './lib/log-worker/modules/retrieve'
+require './lib/log-worker/modules/report'
 require './lib/log-worker/modules/path_factory'
 
 class Backup
 	include Retrieve	
+	include Report
 end
