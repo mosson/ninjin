9556d6f409c7a69f0a460a1702722eab09751e1e
a-munakata <a.munakata@4digit.jp>
2013-07-05 16:30:07 +0900

    seeds.rb : スケールアウトに対応

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,30 +3,38 @@ require 'active_record'
 
 data = []
 files = []
-dir = Dir.new("./db/seeds")
-
-dir.each do |file|
-	if file.match(/[0-9]{8}/)
-		CSV.foreach("#{dir.path}/#{file}") { |entry|	
-			data << {
-				:entry => entry[0],
-				:timestamp => entry[1],		
-				:environment => entry[2],
-				:error_status => entry[3],
-				:github_issued => entry[4],
-				:closed => entry[5],
-				:updated => Time.now,
-				:ip_address => entry[6]
-			}
-		}
+current_dir = Dir.getwd
+dirs = "#{current_dir}/db/seeds"
+Dir::entries(dirs).each do |dir|
+	if File::ftype("#{current_dir}/db/seeds/#{dir}") == "directory"
+		if dir.match(/-[0-9]/)
+			Dir::entries("#{current_dir}/db/seeds/#{dir}").each do |file|
+				if file.match(/[0-9]{8}/)
+					CSV.foreach("#{current_dir}/db/seeds/#{dir}/#{file}") { |entry|
+						data << {
+							:entry => entry[0],
+							:occurred_at => entry[1],
+							:environment => entry[2],
+							:error_status => entry[3],
+							:is_issued => entry[4],
+							:is_closed => entry[5],
+							:is_updated => Time.now,
+							:ip_address => entry[6]
+						}
+					}					
+				end
+			end
+		end
 	end
 end
 
-data.each do |log|	
+puts "inputting data to database..."
+
+data.each do |log|
 	begin
 		logs = Log.new(log) unless log[:entry].nil?
 		logs.save unless logs.nil?
 	rescue => e
-		puts e	
-	end	
+		puts e
+	end
 end
