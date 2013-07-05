c202d50ebbe57447789b478c99504899fa9c0f6a
a-munakata <a.munakata@4digit.jp>
2013-07-05 16:30:24 +0900

    usageに合わせたパスを返す

--- lib/log-worker/modules/path_factory.rb
+++ lib/log-worker/modules/path_factory.rb
@@ -1,5 +1,5 @@
 class PathFactory
-	attr_accessor :conf
+	attr_accessor :conf, :usage, :env, :dir, :file, :formatted_date, :formatted_day_before
 
 	def initialize
 		@conf 	= YAML.load_file("./config/conf.yaml")
@@ -14,4 +14,13 @@ class PathFactory
 		current_dir = Dir.getwd
 		return "#{current_dir}/tmp/report"
 	end	
+
+	def custom(usage, env, dir, file, formatted_date, formatted_day_before)
+		seeds_path = "#{Dir.getwd}/db/seeds/#{env}/#{file.sub(".log", "")}.csv"
+		usage_path = "#{Dir.getwd}/tmp/#{usage}/#{env}/#{dir}/#{formatted_day_before}-#{formatted_date}.log"
+		
+		seeds_base_name = File.dirname(seeds_path)								 
+		usage_base_name = File.dirname(usage_path)
+		return [eval(usage + "_path"), eval(usage + "_base_name")]
+	end
 end
